54ad2cb31b17abeb0c924d77731aa67b40462c79
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2021-05-12 23:48:06 +0200

    renamed script

--- test/response_tutorial.py
+++ test/response_tutorial.py
@@ -1,6 +1,5 @@
 import requests
 
-#response = requests.get ('http://ec2-3-16-160-97.us-east-2.compute.amazonaws.com:28888/')
 response = requests.get ('http://127.0.0.1:8000/')
 
 resp_dict = response.json()
