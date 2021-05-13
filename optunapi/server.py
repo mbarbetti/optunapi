92e0802b6b91099aaef0d50443a3d02b3ccfde18
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2021-05-13 21:51:19 +0200

    updated server script

--- optunapi/server.py
+++ optunapi/server.py
@@ -1,37 +1,89 @@
+import os
+PATH = os.path.abspath (os.path.dirname (__file__))
+
+import optuna
+from optuna.trial._state import TrialState
+
 from fastapi import FastAPI
 from typing  import Optional
 
+from utils import suggest_from_config
+
+
+
 optunapi = FastAPI()
+
 
 @optunapi.get ('/optunapi/hparams/{model_name}')
 async def read_hparams (model_name: str):
-  info = model_name . split ('_')
-  model    = info[0]
-  sample   = info[1]
-  particle = info[2]
-  return {
-           'model'    : model    ,
-           'sample'   : sample   ,
-           'particle' : particle ,
-         }
+  study_name = os.path.join (PATH, 'db', model_name)
+  storage_name = 'sqlite:///{}.db' . format (study_name)
+  study = optuna.create_study ( 
+                                study_name = study_name ,
+                                storage = storage_name  ,
+                                load_if_exists = True   ,
+                              )
+  
+  trial = study.ask()
+  config_file = '{}/config/{}.yaml' . format (PATH, model_name)
+  suggest_from_config (trial, configuration = config_file)
+
+  trial_id = study.trials[-1].number
+  params   = study.trials[-1].params
+  running_trials = study.get_trials (
+                                      deepcopy = False,
+                                      states = (TrialState.RUNNING,)
+                                    )
+  num_running_trials = len(running_trials)
+
+  response = {
+               'model_name' : model_name  ,
+               'trial_id'   : trial_id    ,
+               'params'     : params      ,
+               'running_trials' : num_running_trials ,
+             }
+  
+  return response
+
 
 @optunapi.get ('/optunapi/score/{model_name}')
 async def send_score (
-                       model_name : str   ,
-                       score      : float ,
+                       model_name : str            ,
+                       trial_id   : int            ,
+                       score      : float          ,
                        step : Optional[int] = None ,
                      ):
-  info = model_name . split ('_')
-  model    = info[0]
-  sample   = info[1]
-  particle = info[2]
-  version  = info[3]
-  return {
-           'model'    : model    ,
-           'sample'   : sample   ,
-           'particle' : particle ,
-           'version'  : version  ,
-           'score'    : score    ,
-           'step'     : step     ,
-         }
+  study_name = os.path.join (PATH, 'db', model_name)
+  storage_name = 'sqlite:///{}.db' . format (study_name)
+  study = optuna.create_study ( 
+                                study_name = study_name,
+                                storage = storage_name,
+                                load_if_exists = True
+                              )
+
+  study.tell (trial_id, score)
+
+  params = study.trials[trial_id].params
+  best_trial  = study.best_trial.number
+  best_params = study.best_params
+  best_score  = study.best_value
+  completed_trials = study.get_trials (
+                                        deepcopy = False,
+                                        states = (TrialState.COMPLETE,)
+                                      )
+  num_completed = len(completed_trials)
+
+  response = {
+               'model_name'  : model_name  ,
+               'trial_id'    : trial_id    ,
+               'params'      : params      ,
+               'score'       : score       ,
+               'step'        : step        ,
+               'best_trial'  : best_trial  ,
+               'best_params' : best_params ,
+               'best_score'  : best_score  ,
+               'completed_trials' : num_completed ,
+             }
+
+  return response
   
