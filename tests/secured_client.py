c0ac4c76934b4a6c3ac35d418a6c63942218b237
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2021-05-21 16:44:14 +0200

    set default value

--- tests/secured_client.py
+++ tests/secured_client.py
@@ -1,12 +1,14 @@
 import sshtunnel
 import requests
 
+
 with sshtunnel.open_tunnel (
-  ('ec2-3-141-165-243.us-east-2.compute.amazonaws.com', 22),
-  ssh_username = "mabarbet",
-  ssh_pkey = "/home/mabarbet/lb_mabarbet.pem",
-  remote_bind_address = ('127.0.0.1', 8000),
-  local_bind_address  = ('127.0.0.1', 8040)
+  (REMOTE_SERVER_IP, 22),
+  ssh_username = 'mbarbetti',
+  ssh_pkey = '/home/mbarbetti/.ssh/id_rsa',
+  remote_bind_address = (PRIVATE_SERVER_IP, PRIVATE_SERVER_PORT),
+  local_bind_address  = ('127.0.0.1', 10022)
 ) as tunnel:
-  pippo = requests.get ('http://localhost:8040/optunapi/ping')
-  print (pippo.json())
+  ping = requests.get ('http://localhost:10022/optunapi/ping')
+  ping_msg = ping.json()
+  print (ping_msg)
