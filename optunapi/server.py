0ee26b67c42aab7d17808fe61cad79b369e98baf
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2021-05-24 18:54:13 +0200

    added documentation

--- optunapi/server.py
+++ optunapi/server.py
@@ -11,18 +11,55 @@ from typing import Optional
 from utils  import suggest_from_config, create_log_file
 
 
-
 @optunapi.get ('/optunapi/ping')
 async def ping_server():
+  """
+  Ping Server
+  ===========
+
+  Returns the message "The Optuna-server is alive!" if the server is running.
+
+  Parameters
+  ----------
+    None
+
+  Returns
+  -------
+    msg : str
+      A message witnessing that the server is running.
+  """
   msg = 'The Optuna-server is alive!'
   return msg
 
 
-##########################
-##  Read hyperparameters
-##########################
 @optunapi.get ('/optunapi/hparams/{model_name}')
 async def read_hparams (model_name: str):
+  """
+  Read Hyperparameters
+  ====================
+
+  When a machine submits a GET request with path `/optunapi/hparams/{model_name}`,
+  an Optuna study is created (if it's the first request) or loaded (for any other
+  requests) and an ask instance is called. The resulting trial is equipped with a
+  set of hyperparameters and encoded to the HTTP response together with the name
+  of the optimization session, the trial identifier, the number of running trials 
+  and the number of completed trials.
+
+  Parameters
+  ----------
+    model_name : str (path parameter)
+      Name of the optimization session for which one asks for hyperparameters.
+
+  Returns
+  -------
+    response : dict (HTTP response)
+      Dictionary with the following items:
+        - `model_name` > Name of the optimization session
+        - `trial_id` > Number identifying the created trial
+        - `params` > Current set of values for the hyperparameters
+        - `running_trials` > Number of running trials
+        - `completed_trials` > Number of completed trials
+  """
   study_name = os.path.join (PATH, 'db', model_name)
   storage_name = 'sqlite:///{}.db' . format (study_name)
   study = optuna.create_study ( 
@@ -64,16 +101,47 @@ async def read_hparams (model_name: str):
   return response
 
 
-################
-##  Send score
-################
 @optunapi.get ('/optunapi/score/{model_name}')
 async def send_score (
                        model_name : str            ,
                        trial_id   : int            ,
                        score      : float          ,
-                       step : Optional[int] = None ,
                      ):
+  """
+  Send Score
+  ==========
+
+  When a machine submits a GET request with path `/optunapi/score/{model_name}?trial_id=TRIAL_ID&score=SCORE`,
+  an Optuna study is loaded and its trial `TRIAL_ID` is finished with score `SCORE` calling a tell instance.
+  The corresponding HTTP response encodes the name of the optimization session, `TRIAL_ID`, the tested set of
+  hyperparameters, `SCORE`, the number identifying the best trial, the best set of hyperparameters, the best 
+  score, the number of running trials and the number of completed trials.
+
+  Parameters
+  ----------
+    model_name : str (path parameter)
+      Name of the optimization session for which one asks for hyperparameters.
+
+    trial_id : int (query parameter)
+      Number identifying the tested trial.
+
+    score : float (query parameter)
+      Score obtained with the set of hyperparameters tested.
+
+  Returns
+  -------
+    response : dict (HTTP response)
+      Dictionary with the following items:
+        - `model_name` > Name of the optimization session
+        - `trial_id` > Number identifying the tested trial
+        - `params` > Tested set of values for the hyperparameters
+        - `score` > Score obtained with the tested set of hyperparameters
+        - `best_trial_id` > Number identifying the best trial
+        - `best_params` > Best set of values for the hyperparameters
+        - `best_score` > Score obtained with the best set of hyperparameters
+        - `running_trials` > Number of running trials
+        - `completed_trials` > Number of completed trials
+  """
   study_name = os.path.join (PATH, 'db', model_name)
   storage_name = 'sqlite:///{}.db' . format (study_name)
   study = optuna.create_study ( 
@@ -108,7 +176,6 @@ async def send_score (
                'trial_id'    : trial_id    ,
                'params'      : params      ,
                'score'       : score       ,
-               'step'        : step        ,
                'best_trial'  : best_trial  ,
                'best_params' : best_params ,
                'best_score'  : best_score  ,
